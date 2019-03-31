cc97ee152ce1e3af04ca40a9f50102100ac7d02d
Nawarian <nickolas@phpsp.org.br>
2019-03-31 12:23:36 +0200

    removing top variables from home page

--- source/_partials/home/main.blade.php
+++ source/_partials/home/main.blade.php
@@ -1,12 +1,3 @@
-@php
-$episodes = [
-    'Entrevistas' => $latestEpisodesPerCategory['Entrevista'],
-    'Drops' => $latestEpisodesPerCategory['Drops'],
-    'News' => $latestEpisodesPerCategory['News'],
-];
-
-@endphp
-
 <section class="content">
     <h1 class="heading heading__primary">
         Última Transcrição
