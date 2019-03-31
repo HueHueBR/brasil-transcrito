c8d66ddbfdb4d43c83bf632765bbd282649f6bc5
Nawarian <nickolas@phpsp.org.br>
2019-03-31 13:40:12 +0200

    Adding 1400x1400 favicon for feed image

--- config/meta.production.php
+++ config/meta.production.php
@@ -3,5 +3,5 @@
 $meta = require __DIR__ . '/meta.php';
 
 return array_merge($meta, [
-    'image' => 'https://brasiltranscrito.huehue.eu/assets/images/icons/favicon-180x180.png',
+    'image' => 'https://brasiltranscrito.huehue.eu/assets/images/icons/favicon-1400x1400.png',
 ]);
